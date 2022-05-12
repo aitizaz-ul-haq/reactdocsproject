36dadaaf40599ba78bcbd6fde4dc06ea305783c8
unknown <atz.softprgmr@gmail.com>
2022-05-12 12:52:24 +0500

    went through the props

--- src/docs/main_concepts/components_and_props.js
+++ src/docs/main_concepts/components_and_props.js
@@ -52,6 +52,116 @@ const element = <Welcome name="Sara" />;
 
 // Extracting Components------------------------------------------------->
 // Don’t be afraid to split components into smaller components.
+// For example, consider this Comment component:
+// function Comment(props) {
+//     return (
+//       <div className="Comment">
+//         <div className="UserInfo">
+//           <img className="Avatar"
+//             src={props.author.avatarUrl}
+//             alt={props.author.name}
+//           />
+//           <div className="UserInfo-name">
+//             {props.author.name}
+//           </div>
+//         </div>
+//         <div className="Comment-text">
+//           {props.text}
+//         </div>
+//         <div className="Comment-date">
+//           {formatDate(props.date)}
+//         </div>
+//       </div>
+//     );
+//   }
+
+// It accepts author (an object), text (a string), and date (a date) as props, and describes a comment on a social media website.
+// This component can be tricky to change because of all the nesting, and it is also hard to reuse individual parts of it. 
+// Let’s extract a few components from it.
+
+
+// First, we will extract Avatar:
+// function Avatar(props) {
+//     return (
+//       <img className="Avatar"      
+//       src={props.user.avatarUrl}      
+//       alt={props.user.name}  />  
+//       );
+//   }
+
+
+// The Avatar doesn’t need to know that it is being rendered inside a Comment.
+// This is why we have given its prop a more generic name: user rather than author.
+// We recommend naming props from the component’s own point of view rather than the context in which it is being used.
+
+// We can now simplify Comment a tiny bit:
+// function Comment(props) {
+//     return (
+//       <div className="Comment">
+//         <div className="UserInfo">
+//           <Avatar user={props.author} />        <div className="UserInfo-name">
+//             {props.author.name}
+//           </div>
+//         </div>
+//         <div className="Comment-text">
+//           {props.text}
+//         </div>
+//         <div className="Comment-date">
+//           {formatDate(props.date)}
+//         </div>
+//       </div>
+//     );
+//   }
+
+// Next, we will extract a UserInfo component that renders an Avatar next to the user’s name:
+// function UserInfo(props) {
+//     return (
+//       <div className="UserInfo">      
+//          <Avatar user={props.user} />      
+//         <div className="UserInfo-name">        
+//               {props.user.name}      
+//         </div>    
+//     </div>  
+//     );
+//   }
+
+// This lets us simplify Comment even further:
+// function Comment(props) {
+//     return (
+//       <div className="Comment">
+//         <UserInfo user={props.author} />      
+//         <div className="Comment-text">
+//            {props.text}
+//         </div>
+//         <div className="Comment-date">
+//           {formatDate(props.date)}
+//         </div>
+//       </div>
+//     );
+//   }
+
+
+// Props are Read-Only----------------------------------------------------->
+// Whether you declare a component as a function or a class, it must never modify its own props. Consider this sum function:
+// function sum(a, b) {
+//     return a + b;
+//   }
+// Such functions are called “pure” because they do not attempt to change their inputs, 
+// and always return the same result for the same inputs.
+
+// In contrast, this function is impure because it changes its own input:
+// function withdraw(account, amount) {
+//     account.total -= amount;
+//   }
+
+
+// React is pretty flexible but it has a single strict rule:
+// All React components must act like pure functions with respect to their props.
+
+
+// Of course, application UIs are dynamic and change over time. 
+// In the next section, we will introduce a new concept of “state”. State allows React components to change their output over time 
+// in response to user actions, network responses, and anything else, without violating this rule.
 
 const ComponentsAndProps = () => {
     return(
